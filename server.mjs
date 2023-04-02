c96aeceb7d9e97679c28b63ef2d624014ff1fa96
Aziz Solihin <azizslhn45@gmail.com>
2023-04-03 01:28:31 +0700

    test

--- server.mjs
+++ server.mjs
@@ -1,6 +1,28 @@
-import { createServer } from 'http';
+import { createServer } from "http";
+import env from "dotenv";
+import { Sequelize } from "sequelize";
+
+env.config();
+
+const db = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    dialect: "mysql",
+  }
+);
+
+try {
+  await db.authenticate();
+  console.log("Database connected");
+  // await User.sync();
+} catch (err) {
+  console.error(err);
+}
 
 createServer((req, res) => {
-  res.write('Hello World!');
+  res.write("Hello World!");
   res.end();
 }).listen(process.env.PORT);
